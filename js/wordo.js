d2ee4df16bc6ec9676b0dcb8ab0240daa029a815
Chomppye <202090366+Chomppye@users.noreply.github.com.>
2025-04-16 12:58:11 -0500

    add function to fetch random words from a set lists and lengths

---
+++ js/wordo.js
@@ -0,0 +1,7 @@
+import { fetchWord } from "./wordFetcher.js"
+
+const menuBtn = document.querySelector("#menuBtn")
+const hintBtn = document.querySelector("#hintBtn")
+const restartBtn = document.querySelector("#restartBtn")
+
+console.log(fetchWord(8))
