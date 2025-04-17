52433413030a8c5e6854e0855191e3ec1111031c
Chomppye <202090366+Chomppye@users.noreply.github.com.>
2025-04-17 14:05:12 -0500

    add class to show the row number

--- js/modules/createRows.js
+++ js/modules/createRows.js
@@ -1,12 +1,13 @@
 const rowDiv = document.querySelector("#rows-container")
 
 export function createRows(perRow = 4) {
-    for (let i = 0; i < 6; i++) {
+    for (let i = 1; i < 7; i++) {
            for (let j = 0; j < perRow; j++) {
             let newDiv = document.createElement("div")
             newDiv.classList.add("box")
             newDiv.classList.add(`rows-${perRow}`)
             newDiv.classList.add(`scale-${perRow}`)
+            newDiv.classList.add(`${i}-row`)
             rowDiv.appendChild(newDiv)
         }
     }
